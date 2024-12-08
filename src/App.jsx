6f8b3d0122632347df44d16ccdc6ff03739a53fd
shivani_simpleDev <shivi.solanki29@gmail.com>
2024-12-08 23:22:13 +0100

    ep-16 redux,axios,cors, login working going to feed page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 import { BrowserRouter, Route, Routes } from "react-router"
-import Body from "./Body";
-import Login from "./Login"
-import Profile from "./Profile"
-import Feed from "./Feed"
-
+import Body from "./Components/Body";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import Feed from "./Components/Feed";
+import { Provider } from "react-redux";
+import appStore from "./Utils/appStore";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Body />}>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/profile" element={<Profile />}/>
-          <Route path="/feed" element={<Feed />}/>
-          <Route path="/test" element= {<div>Test page</div>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/test" element={<div>Test page</div>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/Components/Login.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable no-unused-vars */
+// import React from 'react'
+import axios from "axios";
+import { useState } from "react";
+import { BASE_URL } from "../Utils/constant";
+import { useDispatch } from "react-redux";
+import { addUser } from "../Utils/userSlice";
+import { useNavigate } from "react-router";
+
+const Login = () => {
+  const [email, setEmail] = useState("akshay@gmail.com");
+  const [password, setPassword] = useState("Akshay@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center mt-28">
+      <div className="card bg-base-300 text-neutral-content w-96">
+        <div className="card-body items-center text-center">
+          <h2 className="carditle">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">EmailId:</span>
+              </div>
+              <input
+                type="text"
+                className="input input-bordered w-full max-w-xs"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Accept
+            </button>
+            <button className="btn btn-ghost">Deny</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
